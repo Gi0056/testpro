96dd30aef881ff67c489b02786211960fa373a03
Gi0056 <anathlete@icloud.com>
2020-03-05 18:52:11 +0900

    create a new branch.

--- testpro/ViewController.swift
+++ testpro/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
 //        adjfiahsd;flkhalkjgiahsohg;oiahfdg
+        let gi = "I am Gi"
+        
+        print(gi)
     }
 
 
