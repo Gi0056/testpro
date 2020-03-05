28109c427b15eaa7fa303e5bf088da3546b2f1cf
Gi0056 <anathlete@icloud.com>
2020-03-05 19:09:58 +0900

    add some program.

--- testpro/ViewController.swift
+++ testpro/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
 //        adjfiahsd;flkhalkjgiahsohg;oiahfdg
+        
+        let branch = 123323
+        print(branch)
     }
 
 
