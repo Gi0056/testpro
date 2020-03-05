f79cbebf1dfd8b4541d57c4b2ff0e4b85dc0cfd8
Gi0056 <anathlete@icloud.com>
2020-03-05 18:46:51 +0900

    Initial Commit

---
+++ testpro/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testpro
+//
+//  Created by G I on 2020/03/05.
+//  Copyright © 2020 G I. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
